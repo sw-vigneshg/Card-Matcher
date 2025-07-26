4009c38950d49aba06dc8b8d85d942dcb7ca9f94
SWT-LTOP-742\Vignesh <vignesh.g@theunitytech.com>
2025-07-27 00:25:44 +0530

    Working on Game Logic ( Card flip, card comparison )

--- Assets/Scripts/CardHandler.cs
+++ Assets/Scripts/CardHandler.cs
@@ -1,15 +1,55 @@
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardHandler : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer CardBg;
-    [SerializeField] private CardData MyCardData;
+    public CardData MyCardData;
+    [SerializeField] private SpriteRenderer CardFront;
+    [SerializeField] private TMP_Text CardNameText;
+    private GameManager _GameManager;
+    //[SerializeField] private Animator FlipAnimator;
 
-    private void Awake()
+    private void OnEnable()
     {
-        if (CardBg == null)
-            CardBg = GetComponent<SpriteRenderer>();
+        _GameManager = GameManager.Instance;
+        CardFront.gameObject.SetActive(false);
+        //FlipAnimator.SetBool("FlipFront", false);
+        //FlipAnimator.SetBool("FlipBack", false);
+    }
+
+    public void AssignCardData(CardData data)
+    {
+        MyCardData = data;
+    }
+
+    public void AssignCardNameText()
+    {
+        CardNameText.text = MyCardData.CardName;
+    }
+
+    private void OnMouseDown()
+    {
+        if (MyCardData.IsFlipped)
+            return;
+
+        MyCardData.IsFlipped = true;
+
+        //FlipAnimator.SetBool("FlipFront", true);
+
+        CardFront.gameObject.SetActive(true);
+        _GameManager.ValidateSelectedCards(this);
+    }
+
+    public void OnMatch(Color color)
+    {
+        CardFront.color = color;
+    }
+
+    public void OnReset()
+    {
+        MyCardData.IsFlipped = false;
+        CardFront.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,23 @@
-using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    [SerializeField] private CardHandler CardPrefab;
     [SerializeField] private List<CardHandler> CardObjects = new();
     [SerializeField] private CardData[] AllCardData;
 
     [SerializeField] private int RowCount;
     [SerializeField] private int ColoumnCount;
-    private Vector3 CardPosition;
 
-    public Color CardBack;
-    public Color CardFront;
-    public Color WrongMatch;
-    public Color CorrectMatch;
+    [SerializeField] private Color CardFront;
+    [SerializeField] private Color WrongMatch;
+    [SerializeField] private Color CorrectMatch;
+
+    [SerializeField] private PoolManager PoolManager;
+
+    [SerializeField] private List<CardHandler> SelectedCards = new();
 
     private void Awake()
     {
@@ -30,28 +29,83 @@ public class GameManager : MonoBehaviour
         GenerateCards();
     }
 
-    private void GenerateCards()
+    public void GenerateCards()
     {
         if (CardObjects.Count > 0)
+        {
             CardObjects.Clear();
+            Debug.LogError("Card Objects list is not Empty");
+        }
 
-        float SpacingX = 2f;
-        float SpacingY = 2f;
+        int cardIndex = 0;
+        CardData tempData = new CardData();
+
+        float spaceX = 1.3f;
+        float spaceY = 1.5f;
+        Vector3 position;
 
         for (int i = 0; i < RowCount; i++)
         {
             for (int j = 0; j < ColoumnCount; j++)
             {
-                CardPosition.x = i * SpacingX;
-                CardPosition.y = j * SpacingY;
-                CardPosition.z = 0f;
+                position.x = i * spaceX;
+                position.y = j * spaceY;
+                position.z = 0f;
 
-                GameObject card = Instantiate(CardPrefab.gameObject, CardPosition, Quaternion.identity, this.transform);
-                CardHandler cardHandler = card.GetComponent<CardHandler>();
-                if (cardHandler != null && !CardObjects.Exists(x => x == cardHandler))
+                CardHandler card = PoolManager.GetCards();
+                if (card != null)
                 {
-                    CardObjects.Add(cardHandler);
+                    card.transform.parent = this.transform;
+                    card.transform.localPosition = position;
+
+                    tempData = new()
+                    {
+                        CardIndex = AllCardData[cardIndex].CardIndex,
+                        CardName = AllCardData[cardIndex].CardName,
+                        IsFlipped = false,
+                    };
+                    card.AssignCardData(tempData);
+                    cardIndex++;
+                    cardIndex = cardIndex <= AllCardData.Length - 1 ? cardIndex : 0;
+
+                    CardObjects.Add(card);
                 }
+            }
+        }
+        ShuffleCards();
+    }
+
+    private void ShuffleCards()
+    {
+        int randomIndex;
+        for (int i = 0; i < CardObjects.Count; i++)
+        {
+            randomIndex = Random.Range(0, CardObjects.Count);
+            (CardObjects[i].MyCardData, CardObjects[randomIndex].MyCardData) = (CardObjects[randomIndex].MyCardData, CardObjects[i].MyCardData);
+        }
+
+        foreach (CardHandler card in CardObjects)
+        {
+            card.AssignCardNameText();
+        }
+    }
+
+    public void ValidateSelectedCards(CardHandler card)
+    {
+        if (card == null)
+            return;
+
+        if (!SelectedCards.Contains(card))
+            SelectedCards.Add(card);
+
+        if (SelectedCards.Count > 1)
+        {
+            bool isNotMatched = SelectedCards.Exists(x => x.MyCardData.CardIndex != card.MyCardData.CardIndex);
+            foreach (CardHandler cards in SelectedCards)
+            {
+                cards.OnMatch(isNotMatched ? WrongMatch : CorrectMatch);
+                if (isNotMatched)
+                    cards.OnReset();
             }
         }
     }
