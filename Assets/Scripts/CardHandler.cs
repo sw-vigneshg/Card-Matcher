11792e4ebac8d80345a96e3d5d3d1ab857720ab8
SWT-LTOP-742\Vignesh <vignesh.g@theunitytech.com>
2025-07-26 18:52:49 +0530

    Card Generation and Card Prefab setup.

--- Assets/Scripts/CardHandler.cs
+++ Assets/Scripts/CardHandler.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class CardHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private SpriteRenderer CardBg;
+    [SerializeField] private CardData MyCardData;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (CardBg == null)
+            CardBg = GetComponent<SpriteRenderer>();
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+
+    [SerializeField] private CardHandler CardPrefab;
+    [SerializeField] private List<CardHandler> CardObjects = new();
+    [SerializeField] private CardData[] AllCardData;
+
+    [SerializeField] private int RowCount;
+    [SerializeField] private int ColoumnCount;
+    private Vector3 CardPosition;
+
+    public Color CardBack;
+    public Color CardFront;
+    public Color WrongMatch;
+    public Color CorrectMatch;
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        GenerateCards();
     }
+
+    private void GenerateCards()
+    {
+        if (CardObjects.Count > 0)
+            CardObjects.Clear();
+
+        float SpacingX = 2f;
+        float SpacingY = 2f;
+
+        for (int i = 0; i < RowCount; i++)
+        {
+            for (int j = 0; j < ColoumnCount; j++)
+            {
+                CardPosition.x = i * SpacingX;
+                CardPosition.y = j * SpacingY;
+                CardPosition.z = 0f;
+
+                GameObject card = Instantiate(CardPrefab.gameObject, CardPosition, Quaternion.identity, this.transform);
+                CardHandler cardHandler = card.GetComponent<CardHandler>();
+                if (cardHandler != null && !CardObjects.Exists(x => x == cardHandler))
+                {
+                    CardObjects.Add(cardHandler);
+                }
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class CardData
+{
+    public int CardIndex = -1;
+    public string CardName = string.Empty;
+    public bool IsFlipped = false;
 }
